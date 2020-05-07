b1e4c148ae91f4fcf89799065e8aef07643ca0d8
lloau <joaomarcospiferupc@gmail.com>
2020-05-08 01:45:04 +0200

    Snake0805v1
    Main structure

---
+++ Snake_0805v1.py
@@ -0,0 +1,69 @@
+# imports
+
+import pygame
+
+# Global Variables
+
+    # Window properties
+
+
+
+class Snake():
+
+
+    def __init__(self):
+
+        pass
+
+    def Draw(self):
+
+        pass
+    
+    def Move(self, key):
+        
+        pass
+
+    def Eating(self,apple)
+
+        pass
+
+    def Colide(self, array):
+
+        pass
+    d
+
+
+class Apple():
+
+    def __init__(self):
+
+        pass
+    
+    def Draw(self):
+
+        pass
+    
+    pass
+
+
+
+class Screens():
+
+    def __init__(self):
+
+        pass
+
+    pass
+
+
+
+
+
+
+if __name__=="__main__":
+
+    a = 1
+
+    h =  suma(1,a)
+
+    pass
